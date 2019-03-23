240b54a80b9ff602cc881d7c5a5959abd394056e
Luiz Felipe Lersch <luizflersch@gmail.com>
2019-03-23 17:35:55 -0300

    Enunciados

--- T1.hs
+++ T1.hs
@@ -1,6 +1,6 @@
 ﻿import Data.Char
 
--- 1)
+-- 1) Crie uma função isVowel :: Char -> Bool que verifique se um caracter é uma vogal ou não.
 
 isVowel :: Char -> Bool
 
@@ -9,7 +9,7 @@ isVowel x = if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x ==
 
 
 
--- 2)
+-- 2) Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista
 
 
 addComma  :: [String] -> [String]
@@ -18,7 +18,9 @@ addComma x = map( ++ ",") x
 
 
 
--- 3)
+-- 3)Crie uma função htmlListItems :: [String] -> [String], que receba uma lista de strings e retorne outra lista 
+--	contendo as strings formatadas como itens de lista em HTML.
+--	Resolva este exercício COM e SEM funções anônimas (lambda). Exemplo de uso da função:
 
 
 
@@ -44,7 +46,8 @@ htmlListItemsCom x = map(\str -> "<LI>" ++ str ++ "</LI>" )x
 
 
 
--- 4)
+-- 4) Defina uma função que receba uma string e produza outra retirando as vogais, conforme os exemplos abaixo.
+--	 Resolva este exercício COM e SEM funções anônimas (lambda).
 
 
 
@@ -73,7 +76,9 @@ noVowelsCOM x = filter(\str -> str `notElem` "aeiouAEIOU")x
 
 
 
--- 5)
+-- 5) Defina uma função que receba uma string, possivelmente contendo espaços, e que retorne outra string 
+--	substituindo os demais caracteres por '-', mas mantendo os espaços. Resolva este exercício COM e SEM funções 
+--	anônimas (lambda). Exemplos:
 
 -- SEM FUNÇÕES ANONIMAS
 
@@ -94,22 +99,33 @@ onlySpaceCOM :: String -> String
 onlySpaceCOM x = map(\c -> if (c == ' ') then ' ' else '-')x
 
 
--- 6)
+-- 6) Escreva uma função firstName :: String -> String que, dado o nome completo de uma pessoa, 
+--	obtenha seu primeiro nome. Suponha que cada parte do nome seja separada por um espaço e que não existam espaços
+--	no início ou fim do nome. Dica: estude funções pré-definidas em Haskell (List operations -> Sublists) em 
+--	http://hackage.haskell.org/package/base-4.10.1.0/docs/Prelude.html#g:18. Exemplos de uso da função:
 
 firstName :: String -> String
 firstName x = takeWhile(/=' ')x
 
--- 7)
+-- 7) Escreva uma função isInt :: String -> Bool que verifique se uma dada string só contém dígitos de 0 a 9.
+--	Exemplos:
 
 isInt :: String -> Bool
 isInt x = if(length(filter(`notElem` "0123456789")x)) ==0 then True else False
 
--- 8)
+-- 8) Escreva uma função lastName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu último
+--	sobrenome. Suponha que cada parte do nome seja separada por um espaço e que não existam espaços no início
+--	ou fim do nome. Exemplos de uso da função:
 
 lastName :: String -> String
 lastName x = reverse(takeWhile(/=' ')(reverse x))
 
--- 9)
+-- 9) Escreva uma função userName :: String -> String que, dado o nome completo de uma pessoa, 
+--	crie um nome de usuário (login) da pessoa, formado por: primeira letra do nome seguida do sobrenome,
+--	tudo em minúsculas. Dica: estude as funções pré-definidas no módulo Data.Char, para manipulação de maiúsculas
+--	e minúsculas. Você precisará carregar este módulo usando import Data.Char no interpretador ou no início
+--	do arquivo do programa.
+
 userName :: String -> String
 userName x = map(toLower) ((take 1 x) ++ (lastName x))
 
@@ -133,7 +149,8 @@ swapLetter c
 encodeName :: String -> String
 encodeName x = map(swapLetter)x
 
--- 11)
+-- 11) Escreva uma função betterEncodeName :: String -> String que substitua vogais em uma string,
+--	conforme este esquema: a = 4, e = 3, i = 1, o = 0, u = 00. Exemplos de uso da função:
 
 swapLetter2 :: Char -> String
 swapLetter2 c
@@ -152,7 +169,9 @@ swapLetter2 c
 betterEncodeName :: String -> String
 betterEncodeName x = concatMap(swapLetter2)x
 
--- 12)
+-- 12) Dada uma lista de strings, produzir outra lista com strings de 10 caracteres, usando o seguinte esquema:
+--	strings de entrada com mais de 10 caracteres são truncadas, strings com até 10 caracteres são completadas
+--	com '.' até ficarem com 10 caracteres. Exemplo:
 
 montaString :: String -> String
 montaString s = if(length(take 10 s) < 10) then montaString (s++".")	
@@ -162,17 +181,4 @@ montaString s = if(length(take 10 s) < 10) then montaString (s++".")
 
 func :: [String] -> [String]
 func x = map(montaString)x
-
-
-
-
-
-
-
-
-
-
-
-
-
 
