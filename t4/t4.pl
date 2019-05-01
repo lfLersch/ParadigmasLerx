a499cbdd1abf003394f7f18b869cf798ba93c77b
Luiz Felipe Lersch <luizflersch@gmail.com>
2019-05-01 12:40:25 -0300

    2 slayers

--- t4/t4.pl
+++ t4/t4.pl
@@ -1,40 +1,88 @@
-acesso2(X) :-
-  Casas = [_, _, _, _], 	
-  member(casa(pedro, portoAlegre, santaMaria, apartamento), Casas), 
-  member(casa(caren, portoAlegre, santaMaria, apartamento), Casas),	
-  member(casa(henrique, apartamento, apartamento, apartamento), Casas),	
-  member(casa(bia, portoAlegre, santaMaria, apartamento), Casas),
-  member(casa(adriano, santaMaria, apartamento, apartamento), Casas),
-  member(casa(alice, portoAlegre, apartamento, apartamento), Casas),
-  member(casa(bernardo, portoAlegre, santaMaria, apartamento), Casas),
-  member(casa(maria, santaMaria, santaMaria, apartamento), Casas),
-  member(casa(_, apartamento,_,apartamento)Casas).
+segunda(pedro,santaMaria).
+segunda(caren,portoAlegre).
+segunda(henrique,apartamento).
+segunda(bia, apartamento).
+segunda(adriano, apartamento).
+segunda(alice, apartamento).
+segunda(bernardo, portoAlegre).
+segunda(maria, apartamento).
 
-apartamentoQuarta(henrique).
-apartamentoQuinta(henrique).
-apartamentoQuinta(adriano).
-apartamentoQuinta(alice).
-apartamentoSexta(pedro).
-apartamentoSexta(caren).
-apartamentoSexta(henrique).
-apartamentoSexta(bia).
-apartamentoSexta(adriano).
-apartamentoSexta(alice).
-apartamentoSexta(bernardo).
-apartamentoSexta(maria).
-santaMariaQuarta(adriano).
-santaMariaQuarta(maria).
-portoAlegreQuinta().
+terca(pedro,santaMaria).
+terca(caren,portoAlegre).
+terca(henrique,portoAlegre).
+terca(bia, portoAlegre).
+terca(adriano, apartamento).
+terca(alice, portoAlegre).
+terca(bernardo, portoAlegre).
+terca(maria, santaMaria).
+
+quarta(pedro,portoAlegre).
+quarta(caren,portoAlegre).
+quarta(henrique,apartamento).
+quarta(bia, portoAlegre).
+quarta(adriano, santaMaria).
+quarta(alice, portoAlegre).
+quarta(bernardo, santaMaria).
+quarta(maria, santaMaria).
+
+quinta(pedro,santaMaria).
+quinta(caren,santaMaria).
+quinta(henrique,apartamento).
+quinta(bia, santaMaria).
+quinta(adriano, apartamento).
+quinta(alice, apartamento).
+quinta(bernardo, portoAlegre).
+quinta(maria, santaMaria).
+
+sexta(pedro,apartamento).
+sexta(caren,apartamento).
+sexta(henrique,apartamento).
+sexta(bia, apartamento).
+sexta(adriano, apartamento).
+sexta(alice, apartamento).
+sexta(bernardo, apartamento).
+sexta(maria, apartamento).
 
 
-assassino(X) :- motivo(X), acesso(X).
+insano(adriano).
+insano(maria).
 
-acesso(X) :- (apartamentoQuinta(X); apartamentoQuarta(X); santaMariaQuarta(X)),
-(apartamentoQuinta(X); apartamentoSexta(X)).
+pobre(bia).
+pobre(pedro).
+pobre(maria).
+pobre(bernardo).
 
-motivo(X) :- loucura(X); dinheiro(X); ciumes(X).
+relacionamento(anita, bernardo).
+relacionamento(caren, bernardo).
+relacionamento(anita, pedro).
+relacionamento(alice, pedro).
+relacionamento(alice, henrique).
+relacionamento(maria, henrique).
+relacionamento(maria, adriano).
+relacionamento(caren, adriano).
 
-loucura(adriano).
-loucura(maria).
-dinheiro(bia).
-ciumes(caren).
+vitima(anita).
+motivo(X, ciumes) :- relacionamento(X,Y), relacionamento(anita,Y).
+motivo(X, loucura) :- insano(X).
+motivo(X, dinheiro) :- pobre(X).
+
+
+
+acesso(X) :- ((quarta(X, apartamento); quarta(X, santaMaria);quinta(X, portoAlegre)),
+(quinta(X, apartamento); sexta(X, apartamento)),
+(terca(X, portoAlegre)));
+
+((quarta(X, apartamento);quinta(X, portoAlegre)),
+(quinta(X, apartamento); sexta(X, apartamento)),
+(quarta(X, santaMaria); terca(X, portoAlegre)));
+
+((quinta(X, apartamento); quarta(X, apartamento); quarta(X, santaMaria);quinta(X, portoAlegre)),
+(sexta(X, apartamento)),
+(terca(X, portoAlegre)));
+
+((quinta(X, apartamento); quarta(X, apartamento);quinta(X, portoAlegre)),
+(sexta(X, apartamento)),
+(quarta(X, santaMaria); terca(X, portoAlegre))).
+
+
+assassino(X,Y) :- motivo(X,Y), acesso(X).
